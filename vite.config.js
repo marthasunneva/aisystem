2bfc7117bf02a76c9a9e9108a734c145b5c5c8a3
marthasunneva <eaa24mshel@students.eaaa.dk>
2025-03-25 14:53:02 +0100

    Version6

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // vite.config.js
 export default defineConfig({
-  base: '/aisystem/', // ⚠️ Must MATCH the repo name exactly!
+  base: '/aisystem/', // Must MATCH the repo name exactly!
   plugins: [react()],
 });
 
